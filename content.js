872b7dea60a55cfa1b3dc46e739574eecab2e0c8
Hanthebot <56969399+Hanthebot@users.noreply.github.com>
2022-06-27 22:04:02 +0700

    0.2.6 Site modifying section covered by try-catch

--- content.js
+++ content.js
@@ -1,14 +1,19 @@
-document.getElementsByClassName('nav_thum')[0].style.display = 'inline-block';
-document.getElementsByClassName('nav_thum')[0].style.width = '198px';
-document.getElementsByClassName('nav_txt')[0].style.display = 'inline-block';
-document.getElementsByClassName('nav_txt')[0].getElementsByTagName('dl')[0].style.display = 'inherit';
-nav_view = document.getElementsByClassName('nav_viewer')[0].id = "nav_viewer";
 function size_reset() {
     nav_width = nav_viewer.offsetWidth;
     document.getElementsByClassName('nav_txt')[0].style.width = (nav_width - 220) + "px";
     console.log(nav_width+'px');
 }
-size_reset();
+
+try {
+    document.getElementsByClassName('nav_thum')[0].style.display = 'inline-block';
+    document.getElementsByClassName('nav_thum')[0].style.width = '198px';
+    document.getElementsByClassName('nav_txt')[0].style.display = 'inline-block';
+    document.getElementsByClassName('nav_txt')[0].getElementsByTagName('dl')[0].style.display = 'inherit';
+    nav_view = document.getElementsByClassName('nav_viewer')[0].id = "nav_viewer";
+    size_reset();
+} catch (err) {
+    console.log(err);
+}
 
 //window.addEventListener('resize', size_reset());
 new ResizeObserver(size_reset).observe(nav_viewer);
